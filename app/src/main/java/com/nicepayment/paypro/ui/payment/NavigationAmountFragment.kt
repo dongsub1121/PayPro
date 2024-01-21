0914eb55c88e59cd0c19f1ad8022fe0b50dfc110
user <ehdtjq1121@naver.com>
2024-01-21 19:15:24 +0900

    same

--- app/src/main/java/com/nicepayment/paypro/ui/payment/NavigationAmountFragment.kt
+++ app/src/main/java/com/nicepayment/paypro/ui/payment/NavigationAmountFragment.kt
@@ -9,11 +9,13 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.textfield.TextInputEditText
 import com.nicepayment.paypro.R
 import com.nicepayment.paypro.databinding.FragmentHomeBinding
 import com.nicepayment.paypro.databinding.FragmentNavigationAmountBinding
+import kotlinx.coroutines.flow.distinctUntilChanged
 
 class NavigationAmountFragment : Fragment() {
     private var _binding: FragmentNavigationAmountBinding? = null
@@ -21,6 +23,7 @@ class NavigationAmountFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var amount: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,12 +36,10 @@ class NavigationAmountFragment : Fragment() {
 
         next.setOnClickListener {
             Log.e("Model","next")
-            viewModel.setAmount(_binding!!.editTextAmount.text.toString())
-
+            amount = _binding!!.editTextAmount.text.toString()
+            viewModel.setAmount(amount)
             findNavController().navigate(R.id.action_navigationAmountFragment_to_navigationCodeFragment)
         }
-
-
 
         return root
     }
--- app/src/main/java/com/nicepayment/paypro/ui/payment/NavigationCodeFragment.kt
+++ app/src/main/java/com/nicepayment/paypro/ui/payment/NavigationCodeFragment.kt
@@ -1,60 +1,42 @@
 package com.nicepayment.paypro.ui.payment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.nicepayment.paypro.R
+import com.nicepayment.paypro.databinding.FragmentNavigationAmountBinding
+import com.nicepayment.paypro.databinding.FragmentNavigationCodeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [NavigationCodeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NavigationCodeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentNavigationCodeBinding? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private val binding get() = _binding!!
+    private lateinit var barcode: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_navigation_code, container, false)
+        _binding = FragmentNavigationCodeBinding.inflate(layoutInflater, container, false)
+        val root: View = binding.root
+        val viewModel = ViewModelProvider(this)[PaymentViewModel::class.java]
+        val next: Button = _binding!!.next
+
+        next.setOnClickListener {
+            Log.e("Model","next")
+            barcode = _binding!!.editTextAmount.text.toString()
+            viewModel.setAmount(barcode)
+            findNavController().navigate(R.id.action_navigationCodeFragment_to_navigationLoadingStateFragment)
+        }
+
+        return root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NavigationCodeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NavigationCodeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
